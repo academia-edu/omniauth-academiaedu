b3a0ce0adbd3c26f7d30f26e64c2d919ff682a1e
ryanlower <rpjlower@gmail.com>
2012-07-02 10:47:02 -0700

    Use correct, now implemented, client options and user endpoint

--- lib/omniauth/strategies/academiaedu.rb
+++ lib/omniauth/strategies/academiaedu.rb
@@ -3,38 +3,40 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class AcademiaEdu < OmniAuth::Strategies::OAuth2
-            
+
       option :client_options, {
-        :site => 'http://academia.ac:3000',
-        :authorize_url => 'http://academia.ac:3000/oauth/authorize'
+        :site => 'http://api.academia.edu',
+        :authorize_url => 'http://academia.edu/oauth/authorize'
       }
-      
+
       def request_phase
         super
       end
-      
-      uid { 'ryan1' }
+
+      uid { raw_info['id'] }
 
       info do
         {
-          :name => 'Ryan Lower',
-          :email => 'ryan@academia.edu'
+          :name => raw_info['display_name'],
+          :email => raw_info['email'],
+          :urls => {
+            :profile => raw_info['url']
+          }
         }
       end
 
       extra do
-        # { :raw_info => raw_info }
-        { :raw_info => {:id => 114720} }
+        { :raw_info => raw_info }
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/secret').parsed
+        @raw_info ||= access_token.get('/v0/user.json').parsed
       end
-      
+
       def callback_phase
         super
       end
-      
+
     end
   end
 end
